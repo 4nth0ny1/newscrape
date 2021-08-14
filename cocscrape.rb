2f9bd8f63d7c384f8d52c0c40c1f07e12faae970
4nth0ny1 <acatullo4@gmail.com>
2021-08-14 14:03:20 -0500

    first commit

---
+++ cocscrape.rb
@@ -0,0 +1,31 @@
+require 'nokogiri'
+require 'httparty'
+require 'byebug'
+require 'pry'
+
+def scraper
+    url = 'https://web.dallaschamber.org/Accounting,-Tax-Preparation,-Bookkeeping,-and-Payroll-Services' 
+    unparsed_page = HTTParty.get(url)
+    parse_page = Nokogiri::HTML(unparsed_page)
+    company_array = Array.new
+    company_cards = parse_page.css('.tabbertab')
+    company_cards.each do |company|
+        comp = {
+            company_name: company.css('.ListingResults_All_ENTRYTITLELEFTBOX').text,
+            address: company.css("span[@itemprop = 'street-address']").text,
+            city: company.css("span[@itemprop = 'locality']").text,
+            state: company.css("span[@itemprop = 'region']").text,
+            zip: company.css("span[@itemprop = 'postal-code']").text,
+            # i = 1
+            # while phone.values == '' do 
+            #     puts phone: company.css(".ListingResults_Level#{i}_PHONE1").text, 
+            #     i += 1
+            # end  
+            # url: company.css('.ListingResults_Level4_VISITSITE').css('a')[0].attributes['href'].value
+        }
+        company_array << comp
+    end 
+    puts company_array
+end 
+
+scraper
