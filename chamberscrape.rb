3ece5888e6657b7722f461e4fc56eccb21b6312c
4nth0ny1 <acatullo4@gmail.com>
2021-08-14 15:58:07 -0500

    add phone number

--- chamberscrape.rb
+++ chamberscrape.rb
@@ -10,20 +10,25 @@ def scraper
     company_array = Array.new
     company_cards = parse_page.css('.ListingResults_All_CONTAINER')
     company_cards.each do |company|
+        phone = ""
+        company.css('img').each do |img|
+            if img.attributes["alt"]&.value&.include?("Phone")
+                phone = img.attributes['alt'].value.split(": ")[-1]
+                break
+            end 
+        end 
+
         comp = {
             company_name: company.css('.ListingResults_All_ENTRYTITLELEFTBOX').text,
             address: company.css("span[@itemprop = 'street-address']").text,
             city: company.css("span[@itemprop = 'locality']").text,
             state: company.css("span[@itemprop = 'region']").text,
             zip: company.css("span[@itemprop = 'postal-code']").text,
-            # i = 1
-            # while phone.values == '' do 
-            #     puts phone: company.css(".ListingResults_Level#{i}_PHONE1").text, 
-            #     i += 1
-            # end  
-            # url: company.css('.ListingResults_Level4_VISITSITE').css('a')[0].attributes['href'].value
+            phone: phone
+            url: company.css('.ListingResults_Level4_VISITSITE').css('a').attributes['href'].value
         }
         company_array << comp
+       
     end 
     puts company_array
 end 
